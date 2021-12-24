582a7dfd357cabef8e9b47465b768f8596297029
hiro.anzai <a0dinw.aws@gmail.com>
2021-12-24 08:24:28 +0000

    error message japanese localization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   before_action :require_user_logged_in, only: [:show]
+  before_action :logged_in_index_action, only: [:index]
+  
+  def index
+  end
+  
   def show
     @user = User.find(params[:id])
     @pagy, @likes = pagy(@user.like_restaurants.order(id: :desc), items: 3)
@@ -25,5 +30,15 @@ class UsersController < ApplicationController
   
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  
+  # /users にアクセスされた際のリダイレクト先
+  # 会員登録で登録失敗してブラウザのリロードを押した際のエラーページ防止
+  def logged_in_index_action
+    if logged_in?
+      redirect_to user_path(@current_user)
+    else
+      redirect_to signup_url
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
   post 'guest_login', to: 'guest_sessions#create'
   
   get 'signup', to: 'users#new'
-  resources :users, only: [:show, :create] do
+  resources :users, only: [:index, :show, :create] do
     member do
       get :likes
     end
