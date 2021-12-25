f50abef3500135e29182ded1acd788bae1066513
hiro.anzai <a0dinw.aws@gmail.com>
2021-12-25 23:27:49 +0000

    user model rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
   
   has_many :likes
   has_many :like_restaurants, through: :likes, source: :restaurant
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @user = User.new(name: "user", email: "user@example.com", password: "password", password_confirmation: "password")
+  end
+
+  describe 'nameバリデーション' do
+    it "1文字はOK" do
+      @user.name = 'a'
+      expect(@user.valid?).to eq(true)
+    end
+
+    it "50文字はOK" do
+      @user.name = 'a' * 50
+      expect(@user.valid?).to eq(true)
+    end
+
+    it "空ならNG" do
+      @user.name = nil
+      expect(@user.valid?).to eq(false)
+    end
+
+    it "51文字以上ならNG" do
+      @user.name = 'a' * 51
+      expect(@user.valid?).to eq(false)
+    end
+  end
+
+  describe 'emailバリデーション' do
+    it '255文字以内ならOK' do
+      @user.email = 'a' * 243 + '@example.com'
+      expect(@user.valid?).to eq(true)
+    end
+
+    it 'メールアドレスがsave後に小文字化されていること' do
+      @user.email = 'USER@EXamplE.Com'
+      @user.save
+      expect(@user[:email]).to eq('user@example.com')
+    end
+    
+    it '空ならNG' do
+      @user.email = nil
+      expect(@user.valid?).to eq(false)
+    end
+
+    it '256文字以上ならNG' do
+      @user.email = 'a' * 244 + '@example.com'
+      expect(@user.valid?).to eq(false)
+    end
+
+    it '無効なメールアドレスならOK' do
+      @user.email = 'user.name@example.'
+      expect(@user.valid?).to eq(false)
+    end
+  end
+
+  describe 'passwordバリデーション' do
+    it '6文字以上ならOK' do
+      @user.password_confirmation = @user.password = 'aaaaaa'
+      expect(@user.valid?).to eq(true)
+    end
+    
+    it '空ならNG' do
+      @user.password_confirmation = @user.password = nil
+      expect(@user.valid?).to eq(false)
+    end
+    
+    it '5文字以内ならNG' do
+      @user.password_confirmation = @user.password = 'aaaaa'
+      expect(@user.valid?).to eq(false)
+    end
+  end
+
+end
